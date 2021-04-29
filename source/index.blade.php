f2dda2da78abeddc9384ff87ccfd3434f9e51b12
James Sessford <hello@jamessessford.com>
2021-04-29 09:29:25 +0100

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -23,57 +23,63 @@
         x-data="
         {
             text: '',
-            textArray : ['Alpine JS is Amazing', 'It is Truly Awesome!', 'You Have to Try It!'],
+            textArray : [
+                'writing PHP, JavaScript &amp; CSS',
+                'creating custom reports',
+                'creating users',
+                'developing APIs',
+                'deploying code'
+            ],
             textIndex: 0,
             charIndex: 0,
             pauseEnd: 1500,
             cursorSpeed: 420,
             pauseStart: 20,
-            typeSpeed: 110,
+            typeSpeed: 65,
             direction: 'forward'
         }"
         x-init="(() => {
 
             let typingInterval = setInterval(startTyping, $data.typeSpeed);
 
-            function startTyping(){
-            let current = $data.textArray[ $data.textIndex ];
-            if($data.charIndex > current.length){
+            function startTyping() {
+                let current = $data.textArray[ $data.textIndex ];
+                if($data.charIndex > current.length){
                     $data.direction = 'backward';
                     clearInterval(typingInterval);
                     setTimeout(function(){
-                    typingInterval = setInterval(startTyping, $data.typeSpeed);
-                    }, $data.pauseEnd);
-            }
-
-            $data.text = current.substring(0, $data.charIndex);
-            if($data.direction == 'forward'){
-                $data.charIndex += 1;
-                } else {
-                if($data.charIndex == 0){
-                    $data.direction = 'forward';
-                    clearInterval(typingInterval);
-                    setTimeout(function(){
-
-                        $data.textIndex += 1;
-                        if($data.textIndex >= $data.textArray.length){
-                            $data.textIndex = 0;
-                        }
-
                         typingInterval = setInterval(startTyping, $data.typeSpeed);
-                    }, $data.pauseStart);
+                    }, $data.pauseEnd);
                 }
-                $data.charIndex -= 1;
+
+                $data.text = current.substring(0, $data.charIndex);
+                if($data.direction == 'forward'){
+                    $data.charIndex += 1;
+                } else {
+                    if($data.charIndex == 0){
+                        $data.direction = 'forward';
+                        clearInterval(typingInterval);
+                        setTimeout(function(){
+
+                            $data.textIndex += 1;
+                            if($data.textIndex >= $data.textArray.length){
+                                $data.textIndex = 0;
+                            }
+
+                            typingInterval = setInterval(startTyping, $data.typeSpeed);
+                        }, $data.pauseStart);
+                    }
+                    $data.charIndex -= 1;
                 }
 
             }
 
             setInterval(function(){
-            if($refs.cursor.classList.contains('bg-green-900')){
-                $refs.cursor.classList.remove('bg-green-900');
-            } else {
-                $refs.cursor.classList.add('bg-green-900');
-            }
+                if($refs.cursor.classList.contains('bg-green-900')){
+                    $refs.cursor.classList.remove('bg-green-900');
+                } else {
+                    $refs.cursor.classList.add('bg-green-900');
+                }
             }, $data.cursorSpeed);
         })()
         "
