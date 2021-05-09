fd85b460e585da1109501a785bfef1fa3e986831
James Sessford <hello@jamessessford.com>
2021-05-09 10:19:40 +0100

    wip

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,14 +1,14 @@
 <nav id="js-nav-menu" class="nav-menu hidden lg:hidden fixed top-16 w-full">
     <ul class="my-0">
-        <li class="pl-4">
+        <!-- <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Blog"
                 href="/blog"
                 class="nav-menu__item {{ $page->isActive('/blog') ? 'active text-green-900' : '' }}"
             >Blog</a>
-        </li>
+        </li> -->
 
-        <li class="px-4">
+        <li class="pl-4">
             <a href="https://github.com/jamessessford/"
                 class="nav-menu__item text-green-800 hover:text-green-900">
                 GitHub
