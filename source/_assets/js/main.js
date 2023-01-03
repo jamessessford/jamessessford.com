298ecb4f5772597ab5079543f11c3c6fdf3fb237
James Sessford <hello@jamessessford.com>
2023-01-03 21:37:29 +0000

    wip

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,7 +1,7 @@
 window.axios = require('axios');
 import Vue from 'vue';
 import Search from './components/Search.vue';
-import hljs from 'highlight.js/lib/core';
+// import hljs from 'highlight.js/lib/core';
 import Alpine from 'alpinejs';
 import Typewriter from '@marcreichel/alpine-typewriter';
 
@@ -9,22 +9,22 @@ window.Blazy = require('blazy');
 window.Alpine = Alpine;
 
 // Syntax highlighting
-hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
-hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
-hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
-hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
-hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
-hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
-hljs.registerLanguage('nginx', require('highlight.js/lib/languages/nginx'));
-hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
-hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
-hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'));
-hljs.registerLanguage('toml', require('highlight.js/lib/languages/ini'));
-hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
+// hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+// hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+// hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
+// hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+// hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+// hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
+// hljs.registerLanguage('nginx', require('highlight.js/lib/languages/nginx'));
+// hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+// hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
+// hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'));
+// hljs.registerLanguage('toml', require('highlight.js/lib/languages/ini'));
+// hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 
-document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
-});
+// document.querySelectorAll('pre code').forEach((block) => {
+//     hljs.highlightBlock(block);
+// });
 
 Vue.config.productionTip = false;
 
