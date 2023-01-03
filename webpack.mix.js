92a8fecf7dea2577260c306a6fe52a2ff49fd497
James Sessford <hello@jamessessford.com>
2023-01-03 21:23:05 +0000

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js').vue()
-    .css('source/_assets/css/main.css', 'css/main.css', [
+    .css('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
         require('tailwindcss/nesting'),
         require('tailwindcss'),
