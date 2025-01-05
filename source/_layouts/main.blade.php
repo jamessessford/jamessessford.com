584bf68337cb13f3c6e898044ab105a696336ee0
James Sessford <hello@jamessessford.com>
2025-01-05 14:42:51 +0000

    Cleanup

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -39,15 +39,9 @@
 
                 <div id="vue-search" class="flex flex-1 justify-end items-center">
                     @include('_components.search')
-
-                    @include('_nav.menu')
-
-                    @include('_nav.menu-toggle')
                 </div>
             </div>
         </header>
-
-        @include('_nav.menu-responsive')
 
         <main role="main" class="flex-auto w-full container max-w-4xl mx-auto py-16 px-6">
             @yield('body')
